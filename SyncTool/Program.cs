e41692d7bb4493693a29980ed880c977c7c0a130
justin cosgrove <justin.cosgrove@positiveplugins.com>
2018-05-02 09:07:29 -0400

    Initial commit

---
+++ SyncTool/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sage.Peachtree.API;
+using SyncTool.model;
+
+
+
+
+
+namespace SyncTool
+{
+    class Program
+    {
+
+        private static PeachtreeSession apiSession;
+        private static List<Companies> companies;
+
+
+        static void Main(string[] args)
+        {
+            startSession();
+            setCompanyList();
+
+        }
+
+
+        private static void startSession()
+        {
+            //Starts connection to Sage 50
+            apiSession = new PeachtreeSession();
+            apiSession.Begin(Resource1.appID); //Controlled from the Resource1.resx file, blank will allow for connection only to sample companies           
+
+        }
+
+        private static void setCompanyList()
+        {
+            // Get CompanyIdentifer for Bellwether Garden Supply sample company 
+            CompanyIdentifierList m_companyIdList = apiSession.CompanyList();
+
+          
+            
+
+            CompanyIdentifier c = m_companyIdList.ElementAt<CompanyIdentifier>(0);
+
+
+            Console.WriteLine(c.CompanyName + ":" + c.DatabaseName + ":" + c.ServerName + ":" + c.Guid.ToString() + ":" + c.Path);
+            Console.ReadLine();
+
+
+
+        }
+
+        public static void displayCompanies()
+        {
+            foreach(Companies c in companies)
+            {
+                Console.WriteLine(c.CompanyName + ":" + c.Path + ":" + c.Guid);
+            }
+           
+        }
+
+    }
+}
---
+++ SyncTool/model/Companies.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncTool.model
+{
+    class Companies
+    {
+
+        public string CompanyName { get; set; }
+        public string DatabaseName { get; set; }
+        public string Guid { get; set; }
+        public string Path { get; set; }
+        public string SchemaVersion { get; set; }
+        public string ServerName { get; set; }
+
+        public Companies()
+        {
+            
+        }
+
+        public Companies(string companyName, string databaseName, string guid, string path, string schemaVersion, string serverName)
+        {
+            CompanyName = companyName;
+            DatabaseName = databaseName;
+            Guid = guid;
+            Path = path;
+            SchemaVersion = schemaVersion;
+            ServerName = serverName;
+
+        }
+
+    }
+}
+
